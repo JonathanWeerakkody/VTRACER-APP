d7a72663f650e66d441432a5a52ec732c5b7ad09
Jonathan S. Weerakkody <shilantha2004@yahoo.com>
2025-03-18 15:52:30 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 // pages/_app.js
 import '../styles/globals.css'
 import Head from 'next/head'
+import { LanguageProvider } from '../components/i18n/LanguageContext'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <LanguageProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -12,7 +13,7 @@ function MyApp({ Component, pageProps }) {
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </LanguageProvider>
   ) 
 }
 
