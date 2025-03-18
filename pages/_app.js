946982c9d5c00854207188043abeec94afe38869
Jonathan S. Weerakkody <shilantha2004@yahoo.com>
2025-03-18 16:11:25 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,12 @@
-// pages/_app.js
-import '../styles/globals.css'
-import Head from 'next/head'
-import { LanguageProvider } from '../components/i18n/LanguageContext'
+import '../styles/globals.css';
+import { LanguageProvider } from '../components/i18n/LanguageContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <LanguageProvider>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-      </Head>
       <Component {...pageProps} />
     </LanguageProvider>
-  ) 
+  );
 }
 
-export default MyApp
+export default MyApp;
