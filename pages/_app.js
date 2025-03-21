49af6869ddbcee00ecaccff9e6da58d9a750bbef
Jonathan S. Weerakkody <shilantha2004@yahoo.com>
2025-03-20 23:37:42 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
 import '../styles/globals.css';
+import '../styles/responsive.css';
+import '../styles/animations.css';
 import { LanguageProvider } from '../components/i18n/LanguageContext';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <LanguageProvider>
+      <Head>
+        {/* Google AdSense Script */}
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1234567890123456"
+          crossOrigin="anonymous"
+        />
+        {/* Responsive meta tag */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
       <Component {...pageProps} />
     </LanguageProvider>
   );
